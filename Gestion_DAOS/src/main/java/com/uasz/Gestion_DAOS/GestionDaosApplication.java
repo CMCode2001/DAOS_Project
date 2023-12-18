31890883af8df79ca1c99aa74f2dbda622d39efe
El Abdou Drame <eha.d1@zig.univ.sn>
2023-12-18 20:37:41 +0000

    Initialisation du projet

---
+++ Gestion_DAOS/src/main/java/com/uasz/Gestion_DAOS/GestionDaosApplication.java
@@ -0,0 +1,13 @@
+package com.uasz.Gestion_DAOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDaosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDaosApplication.class, args);
+	}
+
+}
