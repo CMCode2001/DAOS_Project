719af513936d791b8562a7f94a97d1089e506ac2
Cheikh Mbacke Coly <cheikh.mb2022@gmail.com>
2023-12-18 23:23:30 +0000

    Creation Package Repartition et ses classes

---
+++ Gestion_DAOS/src/main/java/com/uasz/Gestion_DAOS/repartition/modele/PER.java
@@ -0,0 +1,14 @@
+package com.uasz.Gestion_DAOS.repartition.modele;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+@DiscriminatorValue("PER")
+public class PER extends Enseignant {
+    private String matricule;
+}
