087bf48dfb3da2636bf805a7a7f411cb8e103dec
Cheikh Mbacke Coly <cheikh.mb2022@gmail.com>
2023-12-19 00:39:08 +0000

    Ajout des Classes du package Maquette et etablissement des relations

--- Gestion_DAOS/src/main/java/com/uasz/Gestion_DAOS/maquette/modele/Cycle.java
+++ Gestion_DAOS/src/main/java/com/uasz/Gestion_DAOS/maquette/modele/Cycle.java
@@ -1,4 +1,25 @@
 package com.uasz.Gestion_DAOS.maquette.modele;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Cycle {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCycle;
+    private String libelleCycle;
+
+    @OneToMany(mappedBy = "cycle")
+    private List<Niveau> niveaux;
+
+
 }
+
+
